448b2c218692823a99bc7569643b522410a76a38
joseph <josephjohndemarco@gmail.com>
2020-10-27 13:34:42 -0400

    Function basics.

---
+++ JavaScript Basics/functions.js
@@ -0,0 +1,23 @@
+/**
+ * Functions
+ */
+
+ // Used to divide our code into reusable parts.
+ // Code between the curly braces is executed each time the function is called.
+
+ // Definition
+ function functionName() {
+   console.log("Hello World");
+ }
+
+ // Invocation
+ functionName();
+
+ // Parameters are variables that act as placeholders for values to put input into a function.  The actual values passed into a function are called arguments.
+
+ function testFun(param1, param2) {
+   console.log(param1, param2);
+ }
+
+ // "Hello" and "World" are arguments.
+ testFun("Hello", "World");
