a5f34d0c42243e38542913db5063e072f4fbd545
joseph <josephjohndemarco@gmail.com>
2020-11-09 09:33:16 -0500

    Solve stringMutation challenge.

---
+++ basic-algorithm-scripting/whereDoIBelong.js
@@ -0,0 +1,19 @@
+/*
+  
+  Given an array, find the index where the provided number should be inserted.
+
+*/
+
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => (a - b));
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) return i;
+  }
+
+  return arr.length;
+
+}
+
+
+getIndexToIns([40, 60], 50);
