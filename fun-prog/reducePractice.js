806595258f880ef54e79307324498a088afb89ae
joseph <josephjohndemarco@gmail.com>
2020-11-28 14:18:14 -0500

    Solve reduce object count mini challenge.

--- fun-prog/reducePractice.js
+++ fun-prog/reducePractice.js
@@ -1,3 +1,4 @@
+// From: https://coursework.vschool.io/array-reduce-exercises/
 // Use reduce to solve the following:
 
 // 1) Turn an array of numbers into a total of all the numbers
@@ -16,3 +17,26 @@ function stringConcat(arr) {
 
 // console.log(stringConcat([1,2,3])); // "123"
 
+// 3) Turn an array of voter objects into a count of how many people voted
+
+function totalVotes(arr) {
+    return arr.reduce((acc, cur) => 
+      acc + cur.voted, 0)
+}
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+// console.log(totalVotes(voters)); // 7
