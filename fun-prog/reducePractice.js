94f79e2df83adf2e23e0f16603cf6b8d06264790
joseph <josephjohndemarco@gmail.com>
2020-11-28 14:04:43 -0500

    Solve reduce string concat mini challenge.

--- fun-prog/reducePractice.js
+++ fun-prog/reducePractice.js
@@ -6,5 +6,13 @@ function total(arr) {
   return arr.reduce((acc, cur) => acc + cur, 0)
 }
 
-console.log(total([1,2,3])); // 6
+//console.log(total([1,2,3])); // 6
+
+//2) Turn an array of numbers into a long string of all those numbers.
+
+function stringConcat(arr) {
+  return arr.reduce((acc, cur) => acc + cur, '')
+}
+
+// console.log(stringConcat([1,2,3])); // "123"
 
