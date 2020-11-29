a53b43179cc9677d58a9d320adffd7185094ceda
joseph <josephjohndemarco@gmail.com>
2020-11-29 08:17:13 -0500

    Solve higher order challenge, FCC

---
+++ fun-prog/lessons/higherOrderFunctions.js
@@ -0,0 +1,27 @@
+/**
+ * Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+ */
+
+/**
+ * We have defined a function named squareList.
+ * You need to complete the code for the squareList function using any combination of map(), filter(), and reduce()
+ * so that it returns a new array containing only the square of
+ * only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it.
+ * An example of an array containing only real numbers is [-3, 4.8, 5, 3, -3.2].
+ */
+
+// const squareList = arr => {
+//   return arr.filter((el) => el > 0 && Number.isInteger(el)).map((el => el**2));
+// };
+
+const squareList = (arr) => {
+  return arr.reduce((acc, num) => {
+    return num > 0 && Number.isInteger(num)
+      ? acc.concat(num**2)
+      : acc
+
+  }, [])
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
