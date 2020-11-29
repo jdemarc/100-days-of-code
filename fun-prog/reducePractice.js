78577bc54f45a3beb92cdb654b4a12fe9d37982b
joseph <josephjohndemarco@gmail.com>
2020-11-29 07:54:29 -0500

    More reduce practice.

--- fun-prog/reducePractice.js
+++ fun-prog/reducePractice.js
@@ -68,3 +68,17 @@ var arrays = [
     [true],
     [4, 5, 6]
 ];
+
+// Tally using reduce
+
+const fruitBasket = ['banana', 'cherry', 'orange', 
+                    'apple', 'cherry', 'orange',
+                    'apple', 'banana', 'cherry',
+                    'orange', 'fig', 'cherry' ];
+
+const count = fruitBasket.reduce( (tally, fruit) => {
+  tally[fruit] = (tally[fruit] || 0) + 1 ;
+  return tally;
+} , {})
+
+console.log(count) // { banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1 }
