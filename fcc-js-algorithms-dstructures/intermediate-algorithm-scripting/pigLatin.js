9a63e4411d9cca625ec9104408d4146e3d8056ce
joseph <josephjohndemarco@gmail.com>
2020-12-10 07:18:19 -0500

    Solve pig latin challenge.

---
+++ fcc-js-algorithms-dstructures/intermediate-algorithm-scripting/pigLatin.js
@@ -0,0 +1,38 @@
+/**
+ * Pig Latin
+ */
+
+function translatePigLatin(str) {
+
+  // Check if the string begins with a vowel.
+  let re = /^[aeiou]/;
+
+  // If the string does begin with a vowel,
+  if (str.match(re)) {
+
+    // return the string + 'way'
+    return str + 'way';
+
+  // Otherwise...
+  } else {
+    let consonantGroup = '';
+
+    // Loop through the string. Add consonants to a string variable.
+    for (let i = 0; i < str.length; i++) {
+      if (!str[i].match(re)) {
+        consonantGroup += str[i];
+
+      // Break when a vowel is found.
+      } else {
+        break;
+      }
+    }
+
+    // Return the word from where the group was broken off, plus the remainder of the word, plus 'ay'.
+    return str.slice(consonantGroup.length) + consonantGroup + 'ay';
+  }
+
+}
+
+translatePigLatin("consonant");
+
