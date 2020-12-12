7a0c1b1c7579198edaa5f1c4dfcfd93523a9374c
joseph <josephjohndemarco@gmail.com>
2020-12-12 07:45:13 -0500

    Add global (g flag) to extract multiple pattenrs.

--- fcc-js-algorithms-dstructures/regularExpressions.js/matchMethod.js
+++ fcc-js-algorithms-dstructures/regularExpressions.js/matchMethod.js
@@ -8,7 +8,7 @@ let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 let result = extractStr.match(codingRegex);
 
-console.log('Extracting with .match():' , result)
+console.log('Extracting with .match(): ', result)
 
 /* 
   .match() syntax is the reverse of .test().
@@ -17,4 +17,10 @@ console.log('Extracting with .match():' , result)
   /regex/.test('string');
 */
 
+// Extract the pattern more than once with the -g flag.
 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< g flag here
+let starResult = twinkleStar.match(starRegex);
+
+console.log('Extracting multiple patterns with g flag: ', starResult);
