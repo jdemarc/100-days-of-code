5f57d6c01ab9ac75042ac34b5325b53974a9a7f8
joseph <josephjohndemarco@gmail.com>
2020-12-12 07:27:08 -0500

    Add literal string matching, lsm with OR.

--- fcc-js-algorithms-dstructures/regularExpressions.js/testMethod.js
+++ fcc-js-algorithms-dstructures/regularExpressions.js/testMethod.js
@@ -5,6 +5,17 @@
 // Returns true or false if the regex 'pattern' is found.
 
 let str = "Hello world."
-let regex = /Hello/;
+let regex = /hello/;
 
-console.log(regex.test(str));
+console.log('Literal string matching: ',regex.test(str));
+
+// Above, we are testing for the literal match of the word 'Hello'.
+// 'hello' returns false.
+
+// You can search for multiple literal strings using the OR operationg, '|'
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let result = petRegex.test(petString);
+
+console.log('Literal string with OR:', result);
