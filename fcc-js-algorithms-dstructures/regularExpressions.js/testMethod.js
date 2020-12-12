9a934e8cbdb788d32cf7129c9176384be2e701d8
joseph <josephjohndemarco@gmail.com>
2020-12-12 07:31:07 -0500

    Add i flag (case ignoring) test.

--- fcc-js-algorithms-dstructures/regularExpressions.js/testMethod.js
+++ fcc-js-algorithms-dstructures/regularExpressions.js/testMethod.js
@@ -14,8 +14,24 @@ console.log('Literal string matching: ',regex.test(str));
 
 // You can search for multiple literal strings using the OR operationg, '|'
 
+/**
+ * OR operator
+ */
+
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/;
 let result = petRegex.test(petString);
 
 console.log('Literal string with OR:', result);
+
+/**
+ * Ignoring case.
+ */
+
+// the 'i' flag.
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i;
+let uncasedResult = fccRegex.test(myString);
+
+console.log('Ignoring case matching: ', uncasedResult);
