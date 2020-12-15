a71b6902402c54d1a3c48544a8b722b37fc884f5
joseph <josephjohndemarco@gmail.com>
2020-12-15 10:48:26 -0500

    Complete sum all primes challenge.

---
+++ fcc-js-algorithms-dstructures/intermediate-algorithm-scripting/sumAllPrimes.js
@@ -0,0 +1,44 @@
+/**
+ * Sum All Primes
+ */
+
+/*
+
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself.
+For example, 2 is a prime number because it is only divisible by 1 and 2.
+In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+*/
+
+function sumPrimes(num) {
+  let sum = 0;
+
+  for (let i = 1; i <= num; i++) {
+    if (isPrime(i)) {
+      console.log(sum);
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+function isPrime(num) {
+
+  if (num <= 1) return false;
+
+  if (num === 2) return true;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+console.log(sumPrimes(10));
