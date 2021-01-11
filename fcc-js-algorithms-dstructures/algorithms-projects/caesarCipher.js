c1c590a502dba897df9b3b504cae2270e0eedb1a
joseph <josephjohndemarco@gmail.com>
2021-01-11 09:10:06 -0500

    Add better solution.

--- fcc-js-algorithms-dstructures/algorithms-projects/caesarCipher.js
+++ fcc-js-algorithms-dstructures/algorithms-projects/caesarCipher.js
@@ -3,24 +3,12 @@
  */
 
 function rot13(str) {
-  console.log(str.charCodeAt(0) - 13);
-  console.log(String.fromCharCode(70));
-
-  let decoded = '';
   let regex = /[A-Z]/g;
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === ' ' || 
-        str[i] === '.' ||
-        str[i] === '!' ||
-        str[i] === '?' ) {
-      decoded += str[i];
-    } else {
-      decoded += String.fromCharCode((str.charCodeAt(i) % 26) + 65)
-    }
-  }
+  return str.replace(regex, e =>
+    String.fromCharCode((e.charCodeAt(0) % 26) + 65)
+  );
 
-  return decoded;
 }
 
 console.log(rot13("SERR PBQR PNZC"));
